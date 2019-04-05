23b382a2bcb683782fe26e62e03ca2558d5129b3
Oskar <o.wasikowski@gmail.com>
2019-04-05 22:56:06 +0200

    first commit

---
+++ src/sample/Main.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private final int primaryWidth = 900;
+    private final int primaryHeight = 700;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../sample/sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, primaryWidth, primaryHeight));
+
+
+
+
+
+
+
+
+
+
+
+
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
